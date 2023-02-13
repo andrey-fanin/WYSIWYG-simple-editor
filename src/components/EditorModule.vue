7cbd0ffcc74bab96d18dbef3f9e632d8eaff701e
Andrey Fanin <andr.fanin16@yandex.ru>
2023-02-13 17:55:27 +0500

    init

---
+++ src/components/EditorModule.vue
@@ -0,0 +1,155 @@
+<script setup>
+import {ref} from 'vue'
+import SvgUndo from "@/assets/SvgUndo";
+import SvgHeading from "@/assets/SvgHeading";
+import SvgParagraph from "@/assets/SvgParagraph";
+import SvgImportImg from "@/assets/SvgImportImg";
+
+const value = ref('');
+const undoStack = ref([]);
+const redoStack = ref([]);
+const editor = ref(null);
+
+function updateValue() {
+  value.value = editor.value.innerHTML;
+  console.log(value.value);
+  undoStack.value.push(value.value);
+}
+
+function undo() {
+  if (undoStack.value.length > 0) {
+    redoStack.value.push(value.value);
+    value.value = undoStack.value.pop();
+    editor.value.innerHTML = value.value;
+  }
+}
+
+function redo() {
+  if (redoStack.value.length > 0) {
+    undoStack.value.push(value.value);
+    value.value = redoStack.value.pop();
+    editor.value.innerHTML = value.value;
+  }
+}
+
+function makeHeading() {
+  document.execCommand('formatBlock', false, 'h1');
+}
+
+function makeParagraph() {
+  document.execCommand('formatBlock', false, 'p');
+}
+
+function importImage() {
+  const url = prompt('Enter image URL:');
+  document.execCommand('insertImage', false, url);
+}
+
+function copyAsHtml() {
+  const selectedHTML = document.createElement('textarea');
+  selectedHTML.value = editor.value.innerHTML;
+  document.body.appendChild(selectedHTML);
+  selectedHTML.select();
+  document.execCommand('copy');
+  selectedHTML.remove();
+  alert('Copied HTML to clipboard');
+}
+
+</script>
+
+<template>
+  <div class="wysiwyg-editor">
+    <div class="toolbar-buttons">
+      <button @click="undo">
+        <svg-undo/>
+      </button>
+      <button class="toolbar-button--redo" @click="redo">
+        <svg-undo/>
+      </button>
+      <button @click="makeHeading">
+        <svg-heading/>
+      </button>
+      <button @click="makeParagraph">
+        <svg-paragraph/>
+      </button>
+      <button @click="importImage">
+        <svg-import-img/>
+      </button>
+      <button class="toolbar-button--copy-html" @click="copyAsHtml">Скопировать HTML</button>
+    </div>
+    <div contenteditable="true" ref="editor" @input="updateValue">
+      {{ value ? null : 'Click and type something!' }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+.wysiwyg-editor {
+  .toolbar-buttons {
+    display: flex;
+    padding: 10px;
+    gap: 12px;
+  }
+
+  button {
+    background-color: #282828;
+    border: 1px solid #282828;
+    border-radius: 4px;
+    padding: 5px 10px;
+    margin-right: 10px;
+    cursor: pointer;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 42px;
+    min-height: 38px;
+    transition: .4s;
+
+    &.toolbar-button--copy-html {
+      background-color: transparent;
+      border: unset;
+      color: #639EFF;
+    }
+
+    &.toolbar-button--redo {
+      transform: scale(-1, 1);
+    }
+
+    &:hover {
+      color: #ccc;
+
+      svg path {
+        transition: .4s;
+        fill: #ccc;
+      }
+    }
+  }
+
+  [contenteditable="true"] {
+    padding: 10px;
+    min-height: 200px;
+    font-size: 18px;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+
+    p {
+      max-width: 620px;
+      margin-block: 30px;
+    }
+
+    h1 {
+      font-size: 31px;
+      line-height: 74%;
+      color: #FFFFFF;
+      margin-block: 46px 33px;
+    }
+
+    img {
+      max-width: 100%;
+    }
+  }
+}
+</style>
